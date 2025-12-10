1918e834949f78fd1eaf9dc7a2b7ef43f903695e
Jack Rudenko <i@madappgang.com>
2025-12-10 17:05:23 +1100

    fix(queries): Update capture names for nvim-treesitter compatibility

--- queries/dingo/highlights.scm
+++ queries/dingo/highlights.scm
@@ -56,51 +56,90 @@
 
 ; Dingo special operators
 "=>" @operator
+
 "?." @operator
+
 "??" @operator
-(error_propagation "?" @operator)
+
+(error_propagation
+  "?" @operator)
 
 ; Lambda pipes (special highlighting)
-(lambda_expression "|" @punctuation.special)
+(lambda_expression
+  "|" @punctuation.special)
 
 ; Types
-(type_declaration (identifier) @type.definition)
-(type_param (identifier) @type.parameter)
-(generic_type (identifier) @type)
-(qualified_type (identifier) @type)
+(type_declaration
+  (identifier) @type.definition)
+
+(type_param
+  (identifier) @type)
+
+(generic_type
+  (identifier) @type)
+
+(qualified_type
+  (identifier) @type)
 
 ; Dingo enum
-(enum_declaration name: (identifier) @type.definition)
-(enum_variant name: (identifier) @constructor)
-(variant_field (identifier) @property)
+(enum_declaration
+  name: (identifier) @type.definition)
+
+(enum_variant
+  name: (identifier) @constructor)
+
+(variant_field
+  (identifier) @property)
 
 ; Match patterns
-(match_expression "match" @keyword.control)
-(variant_match (identifier) @constructor)
+(match_expression
+  "match" @keyword.conditional)
+
+(variant_match
+  (identifier) @constructor)
 
 ; Functions
-(function_declaration name: (identifier) @function)
-(call_expr (identifier) @function.call)
-(call_expr (selector_expr (identifier) @function.method.call))
+(function_declaration
+  name: (identifier) @function)
+
+(call_expr
+  (identifier) @function.call)
+
+(call_expr
+  (selector_expr
+    (identifier) @function.method.call))
 
 ; Parameters
-(param_decl (identifier) @variable.parameter)
-(lambda_param (identifier) @variable.parameter)
+(param_decl
+  (identifier) @variable.parameter)
+
+(lambda_param
+  (identifier) @variable.parameter)
 
 ; Variables
-(let_declaration (identifier) @variable)
-(var_spec (identifier) @variable)
-(const_spec (identifier) @constant)
+(let_declaration
+  (identifier) @variable)
+
+(var_spec
+  (identifier) @variable)
+
+(const_spec
+  (identifier) @constant)
 
 ; Literals
 (int_literal) @number
+
 (float_literal) @number.float
+
 (string_literal) @string
+
 (rune_literal) @character
 
 ; Booleans and nil
 "true" @boolean
+
 "false" @boolean
+
 "nil" @constant.builtin
 
 ; Comments
@@ -124,10 +163,12 @@
 ] @punctuation.delimiter
 
 ; Import path
-(import_spec (string_literal) @string.special)
+(import_spec
+  (string_literal) @string.special)
 
 ; Package name
-(package_clause (identifier) @namespace)
+(package_clause
+  (identifier) @module)
 
 ; Wildcard pattern
 "_" @variable.builtin
